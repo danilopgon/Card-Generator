525710012f4cb96174a94967665b7032eb23656c
danilopgon <113547781+danilopgon@users.noreply.github.com>
2023-05-13 16:38:39 +0000

    generate cards without styles nor extra functions

---
+++ src/app.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-console */
+const getDeck = () => {
+  const cardSuits = ["♦", "♥", "♠", "♣"];
+  const cardValues = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+
+  let newDeck = cardSuits.flatMap(suits => {
+    return cardValues.flatMap(values => {
+      return { value: values, suit: suits };
+    });
+  });
+
+  return newDeck;
+};
+
+const shuffleCards = cardsDeck => {
+  for (let i = cardsDeck.length - 1; i > 0; i--) {
+    let shuffler = Math.floor(Math.random() * (i + 1));
+    let auxCard = cardsDeck[i];
+    cardsDeck[i] = cardsDeck[shuffler];
+    cardsDeck[shuffler] = auxCard;
+  }
+
+  return cardsDeck;
+};
+
+const renderCard = cardsDeck => {
+  const { suit, value } = cardsDeck[cardsDeck.length - 1];
+  const card = document.getElementById("card");
+
+  const suitElement = document.createElement("p");
+  suitElement.textContent = `${suit}`;
+  card.append(suitElement);
+
+  const valueElement = document.createElement("p");
+  valueElement.textContent = `${value}`;
+  card.append(valueElement);
+
+  const invertedSuitElement = document.createElement("p");
+  invertedSuitElement.textContent = `${suit}`;
+  invertedSuitElement.style.transform = "rotate(180deg)";
+
+  card.append(invertedSuitElement);
+};
+
+window.onload = function() {
+  const newDeck = getDeck();
+  shuffleCards(newDeck);
+  renderCard(newDeck);
+};
